c21b2c74ba94a88506dc3f27ed877a34bbf9ec3d
Ray Epps <rayharryepps@gmail.com>
2019-10-11 01:42:26 -0700

    + rename project dynamof

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 
-# iDynamo
+# Dynamof
 A small interface for more easily making calls to dynamo using boto. No bloated ORM - just functions that make creating boto3 dynamo action descriptions easy.
--- dynamof/errors.py
+++ dynamof/errors.py
@@ -1,7 +1,7 @@
 
 
 
-class IDynamoError(Exception):
+class DynamofError(Exception):
    def __init__(self, message):
        super().__init__(message)
    def info(self, add_message):
@@ -9,12 +9,12 @@ class IDynamoError(Exception):
        self.message = f'{self.message}; {add_message}'
        return self
 
-class DatabaseFindError(IDynamoError):
+class DatabaseFindError(DynamofError):
     def __init__(self):
         message = "Could not find entity"
         super().__init__(message)
 
-class UnknownError(IDynamoError):
+class UnknownError(DynamofError):
     def __init__(self):
         message = "Unknown error occured while communicating with dynamo"
         super().__init__(message)
