168b09d3a062e0ee4659006b1e062c869eb0ac69
Ray Epps <rayharryepps@gmail.com>
2019-10-18 21:22:34 -0700

    TDD: Write failing test for Decimal parsing feature

---
+++ test/integration/data_consistency_test.py
@@ -0,0 +1,46 @@
+import json
+import pytest
+from functools import partial
+from boto3 import client
+
+from test.integration.fixtures import db
+
+from dynamof.conditions import attr
+from dynamof.operations import (
+    create,
+    find,
+    add,
+    update,
+    delete,
+    query
+)
+
+def test_numbers_are_not_changed(db):
+    """Asserts that numbers inserted into dynamo are not converted
+    to another type - **namely Decimals**. See https://github.com/boto/boto3/issues/369
+    This test is to assert that dynamof correctly handles this by converting
+    any Decimals that dynamo returns back to a default json parsable property.
+    """
+
+    db(create(
+        table_name='data_const',
+        hash_key='id',
+        allow_existing=True))
+
+    db(add(table_name='data_const', item={
+        'id': 'aaaaaa',
+        'prefrences': {
+            'volume': 9
+        }
+    }))
+
+    user = db(find(table_name='data_const', key={
+        'id': 'aaaaaa'
+    }))
+
+    volume = user.get('item').get('prefrences').get('volume')
+
+    assert volume == 9
+    assert isinstance(volume, int)
+
+    db(delete(table_name='data_const'))
