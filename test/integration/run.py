d8ba6d1beb46ba73b3dd017be81fa400827dcdaa
Ray Epps <rayharryepps@gmail.com>
2019-10-26 15:55:12 -0700

    integration tests - wait for db to start

--- test/integration/run.py
+++ test/integration/run.py
@@ -1,14 +1,43 @@
 #!/usr/bin/env python
 
+import time
+
 import pytest
 
 from test.integration.fixtures import db_func
 
-from dynamof.operations import create
+from dynamof.exceptions import TableDoesNotExistException
+from dynamof.operations import create, describe
+
+DATABASE_UPTIME_WAIT = 3 # seconds
+DATABASE_UPTIME_RETRIES = 6 # times
+
+def wait_for_test_db():
+    """Pings the localstack db with describe requests
+    for {some time} waiting for it to come up...
+    """
+    print('####################################')
+    print('Waiting for database to start.......')
+    print('####################################')
+
+    def try_request(retries):
+        try:
+            db_func(describe(table_name='users'))
+        except TableDoesNotExistException:
+            print('** ------>')
+            print('Beep Boop Beep Boop DYNAMO ready....')
+            print('** ------>')
+            return
+        except Exception:
+            if retries < DATABASE_UPTIME_RETRIES:
+                time.sleep(DATABASE_UPTIME_WAIT)
+                return try_request(retries + 1)
+            raise Exception('Integration tests FAILED - could not connect to dynamo in time')
+
+    return try_request(0)
 
 
 def setup_test_tables():
-
     print('####################################')
     print('Creating tables for integration test')
     print('####################################')
@@ -18,6 +47,6 @@ def setup_test_tables():
         allow_existing=True))
 
 if __name__ == '__main__':
-
+    wait_for_test_db()
     setup_test_tables()
     pytest.main(['test/integration'])
