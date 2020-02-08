f110b1345afad1b6d4f1ba37473432f50ec2acc4
Ray Epps <15269623+rayepps@users.noreply.github.com>
2020-02-08 10:37:38 -0800

    Implement between and begins_with conditions (#5)
    * Implement between condition
    
    * Implement begins_with condition

--- dynamof/core/model.py
+++ dynamof/core/model.py
@@ -2,13 +2,15 @@
 from dynamof.core.utils import immutable
 
 
-def Attr(equals, gt, lt, lt_or_eq, gt_or_eq):
+def Attr(equals, gt, lt, lt_or_eq, gt_or_eq, between, begins_with):
     return immutable({
         'equals': equals,
         'gt': gt,
         'lt': lt,
         'lt_or_eq': lt_or_eq,
-        'gt_or_eq': gt_or_eq
+        'gt_or_eq': gt_or_eq,
+        'between': between,
+        'begins_with': begins_with
     })
 
 def Operation(description, runner):
